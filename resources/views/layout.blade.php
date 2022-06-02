bc0ca1fbb74c947444e41022923fdd9a520a536f
Vitor na Empresa <vitortactic@gmail.com>
2022-06-02 15:51:51 -0300

    plaza-events inicio do site com bootstrap

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Layout</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">   
+
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&family=Roboto:ital,wght@0,100;0,400;1,100;1,300&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Kdam+Thmor+Pro&family=Noto+Sans&family=Roboto:ital,wght@0,100;0,400;1,100;1,300&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Kdam+Thmor+Pro&family=Montserrat:wght@100&family=Noto+Sans&family=Poppins:wght@100&family=Roboto:ital,wght@0,100;0,400;1,100;1,300&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="collapse navbar-collapse" id="navbar">
+            <a href="" class="navbar-brand">Plaza</a>
+        </div>
+
+        <ul class="navbar-nav">
+            <li class="nav-item"><a href="" class="nav-link">Meus Eventos</a></li>
+            <li class="nav-item"><a href="" class="nav-link">Eventos</a></li>
+            <li class="nav-item"><a href="" class="nav-link">Login</a></li>
+            <li class="nav-item"><a href="" class="nav-link">Registrar</a></li>
+        </ul>
+    </nav>
+
+    <div class="container-fluid">  
+            <div id="baner">
+                <h1 class="text-center ">Busque um evento</h1>
+                <form action="">
+                    <input type="text" name="" id="" class="form-control" placeholder="Pesquisar...">
+                </form>
+            </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="col-md-11 offset-md-1 mt-5">
+            <h2>Próximos eventos</h2>
+            <p>Veja os eventos dos próximos dias</p>
+    
+            
+    
+            <div id="cards-eventos" class="row">
+                <div class="card col-md-3">
+                    <img src="imgs/event1.jpg" alt="evento-img" class="image-fluid">
+                    <div class="card-body">
+                        <p>10/09/2022</p>
+                        <h5>Palestra de JS</h5>
+                        <p>X Participantes</p>
+                        <a href="#" class="btn btn-unico">Saber mais</a>
+                    </div>
+                </div>
+
+                <div class="card col-md-3">
+                    <img src="imgs/event1.jpg" alt="evento-img" class="image-fluid">
+                    <div class="card-body">
+                        <p>10/09/2022</p>
+                        <h5>Palestra de JS</h5>
+                        <p>X Participantes</p>
+                        <a href="#" class="btn btn-unico">Saber mais</a>
+                    </div>
+                </div>
+
+                <div class="card col-md-3">
+                    <img src="imgs/event1.jpg" alt="evento-img" class="image-fluid">
+                    <div class="card-body">
+                        <p>10/09/2022</p>
+                        <h5>Palestra de JS</h5>
+                        <p>X Participantes</p>
+                        <a href="#" class="btn btn-unico">Saber mais</a>
+                    </div>
+                </div>
+
+                <div class="card col-md-3">
+                    <img src="imgs/event1.jpg" alt="evento-img" class="image-fluid">
+                    <div class="card-body">
+                        <p>10/09/2022</p>
+                        <h5>Palestra de JS</h5>
+                        <p>X Participantes</p>
+                        <a href="#" class="btn btn-unico">Saber mais</a>
+                    </div>
+                </div>
+
+                <div class="card col-md-3">
+                    <img src="imgs/event1.jpg" alt="evento-img" class="image-fluid">
+                    <div class="card-body">
+                        <p>10/09/2022</p>
+                        <h5>Palestra de JS</h5>
+                        <p>X Participantes</p>
+                        <a href="#" class="btn btn-unico">Saber mais</a>
+                    </div>
+                </div>
+                <div class="card col-md-3">
+                    <img src="imgs/event1.jpg" alt="evento-img" class="image-fluid">
+                    <div class="card-body">
+                        <p>10/09/2022</p>
+                        <h5>Palestra de JS</h5>
+                        <p>X Participantes</p>
+                        <a href="#" class="btn btn-unico">Saber mais</a>
+                    </div>
+                </div>
+
+                <div class="card col-md-3">
+                    <img src="imgs/event1.jpg" alt="evento-img" class="image-fluid">
+                    <div class="card-body">
+                        <p>10/09/2022</p>
+                        <h5>Palestra de JS</h5>
+                        <p>X Participantes</p>
+                        <a href="#" class="btn btn-unico">Saber mais</a>
+                    </div>
+                </div>
+    
+    
+               
+            </div>
+        </div>
+    </div>
+    
+    
+    
+    
+
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
